d1090046793d9b465b41e2c1b0a97e5fff14ad38
JPFusco <85804722+JPFusco@users.noreply.github.com>
2022-02-11 19:26:43 -0300

    feat: protegendo rota home, garantindo que usuarios logados vão direto pra home

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router, Routes, Route, Navigate,
+} from 'react-router-dom';
 import Login from './pages/Login';
 import Home from './pages/Home';
 import GlobalContextProvider from './contexts/GlobalContext';
+import useGlobalContext from './hooks/useGlobalContext';
+
+function RotasProtegidas({ children }: { children: JSX.Element }) {
+  const { authToken } = useGlobalContext();
+
+  if (!authToken) {
+    return <Navigate to="/" />;
+  }
+
+  return children;
+}
+
+function RotasDesprotegidas({ children }: { children: JSX.Element }) {
+  const { authToken } = useGlobalContext();
+
+  if (authToken) {
+    return <Navigate to="/home" />;
+  }
+
+  return children;
+}
 
 function App() {
   return (
@@ -10,8 +33,22 @@ function App() {
       <GlobalContextProvider>
         <Router>
           <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/home" element={<Home />} />
+            <Route
+              path="/"
+              element={(
+                <RotasDesprotegidas>
+                  <Login />
+                </RotasDesprotegidas>
+              )}
+            />
+            <Route
+              path="/home"
+              element={(
+                <RotasProtegidas>
+                  <Home />
+                </RotasProtegidas>
+              )}
+            />
           </Routes>
         </Router>
       </GlobalContextProvider>
